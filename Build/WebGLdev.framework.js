9d7f5c5892bc475b6c28c4862eec58c4b885906a
kbb1025doge <vladimir.a@supadoge.com>
2022-05-19 19:42:32 +0300

    upd
    try .Net 4.x

--- Build/WebGLdev.framework.js
+++ Build/WebGLdev.framework.js
@@ -1342,7 +1342,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 4461376;
+STATICTOP = STATIC_BASE + 4461312;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3404,7 +3404,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 4461376;
+var STATIC_BUMP = 4461312;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
